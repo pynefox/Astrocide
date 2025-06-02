d009d53a5228c2fac3214806566f8126df5c25a2
pynefox <thebaconator2211@gmail.com>
2025-06-01 20:26:51 -0400

    Improved and distinguished the flashlight movement script

--- Astrocide/Assets/Scripts/MoveGear.cs
+++ Astrocide/Assets/Scripts/MoveGear.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class MoveGear : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Transform cameraPosition;
-
+    
+    public Transform gearPosition;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = cameraPosition.position;
-
+        transform.position = gearPosition.position;
     }
 }
